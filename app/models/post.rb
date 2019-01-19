b7bdaea114c0a91e0c2779aa35e346fc535cdcc8
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 20:02:48 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,18 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: { maximum: 50 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :clickbait
-
-  private
-
-  def clickbait
-    keywords = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-    result = false
-    keywords.each do |word|
-      if self.title.include?(word)
-        result = true
-      end
-    end
-    result
-  end
+  validates_with Clickbait
 end
