cb749c79a713c1f4eaed0919f049a64007a3126a
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 19:41:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary, length: { maximum: 50 }
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :title, inclusion: { in: %w(You Won't Believe These True Facts) }
 end
