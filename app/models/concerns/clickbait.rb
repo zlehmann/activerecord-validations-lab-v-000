d16b185b48c2441ed6e99e6797d8f4409facebc8
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 20:04:22 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait.rb
+++ app/models/concerns/clickbait.rb
@@ -1,4 +1,4 @@
-class Clickbait < ActiveRecord::Validator
+class Clickbait < ActiveModel::Validator
   def validate(record)
     keywords = ["Won't Believe", "Secret", "Top [number]", "Guess"]
     result = false
