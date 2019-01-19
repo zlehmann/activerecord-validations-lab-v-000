b34c240d3e525dc1e867ce4d6e18508a0608ae80
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 19:35:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { minimum: 10 }
+  validates :content, length: { minimum: 5 }
   validates :long_summary, length { maximum: 50 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 
 end
