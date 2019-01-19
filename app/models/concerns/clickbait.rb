95556bcfa7106c3b348cfa6085fed0bc317c560e
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 20:03:48 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait.rb
+++ app/models/concerns/clickbait.rb
@@ -1,10 +1,12 @@
-class Clickbait < ActiveRecord::Base
-  keywords = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-  result = false
-  keywords.each do |word|
-    if self.title.include?(word)
-      result = true
+class Clickbait < ActiveRecord::Validator
+  def validate(record)
+    keywords = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    result = false
+    keywords.each do |word|
+      if record.title.include?(word)
+        result = true
+      end
     end
+    result
   end
-  result
 end
